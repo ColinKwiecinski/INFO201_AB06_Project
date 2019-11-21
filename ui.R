2de9822dc249e0fad1503a0bf8383e8d280073a9
colinkwiecinski <colinkwiecinski@gmail.com>
2019-11-20 20:28:40 -0800

    Another merge fix

--- ui.R
+++ ui.R
@@ -8,6 +8,14 @@ shinyUI(fluidPage(
 
     sidebarLayout(
         sidebarPanel(
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+            
+>>>>>>> Stashed changes
+=======
+            
+>>>>>>> Stashed changes
             # Chart selector
             selectInput(
                 inputId = "x_axis",
